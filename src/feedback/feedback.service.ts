1250187333464d0b9b19248b277cd99a7a2d54d5
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2023-01-11 14:51:32 +0700

    D

--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -42,7 +42,6 @@ export class FeedbackService {
           'hotels',
           'hotels.id = feedback.hotelId',
         )
-        .groupBy('feedback.hotelId')
         .getMany();
       const feedbacks = await getRepository(Feedback)
         .createQueryBuilder('feedback')
